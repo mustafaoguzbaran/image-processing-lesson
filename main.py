208e0a2390dec9793a92781e68638d278d59e6be
mustafaoguzbaran <mustafaoguzbaran@gmail.com>
2023-11-12 16:27:59 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+import numpy as np
+
+image = cv.imread("apple.jpg",0)
+image_height = image.shape[0]
+image_width = image.shape[1]
+Histogram =np.zeros([256])
+
+for i in range(0,image_height):
+    for x in range(0,image_width):
+        Histogram[image[i, x]] += 1
+
+plt.plot(Histogram)
+plt.title("Histogram Graph")
+plt.show()
